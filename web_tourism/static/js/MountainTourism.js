f5847b5daba403112608c9fef195aa3ce7dc46b3
Irina <ira.kalachina@yandex.ru>
2025-04-17 14:23:56 +0700

    Фикс навигации

--- web_tourism/static/js/MountainTourism.js
+++ web_tourism/static/js/MountainTourism.js
@@ -51,7 +51,7 @@
                         <a href="/walking_tourism/" className="text-xl font-semibold text-blue-600 hover:underline">
                             <h3>Пеший туризм</h3>
                         </a>
-                        <a href="/water_tourism/" className="text-blue-500 hover:underline">Перейти ></a>
+                        <a href="/walking_tourism/" className="text-blue-500 hover:underline">Перейти ></a>
                     </div>
                     <div className="tourism-item bg-white p-4 rounded-lg shadow-lg">
                         <a href="/water_tourism/" className="text-xl font-semibold text-blue-600 hover:underline">
