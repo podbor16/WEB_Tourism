06d21cb1b45db8aeafc430c08c6892d6f6b1feed
Ирина Калачина <ira.kalachina@yandex.ru>
2025-09-21 22:13:50 +0700

    списки в списках

--- web_tourism/static/js/header.js
+++ web_tourism/static/js/header.js
@@ -1,9 +1,6 @@
 ﻿const Header = () => {
     const [isDropdownOpen, setIsDropdownOpen] = React.useState(false);
     const [theme, setTheme] = React.useState("light");
-    const [isLoginModalOpen, setIsLoginModalOpen] = React.useState(false);
-    const [isRegistering, setIsRegistering] = React.useState(false);
-    const [isChoiceModalOpen, setIsChoiceModalOpen] = React.useState(false);
 
     const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
 
@@ -13,51 +10,88 @@
         document.body.classList.toggle("dark-theme", newTheme === "dark");
     };
 
-    const openChoiceModal = (e) => {
-        e.preventDefault();
-        setIsChoiceModalOpen(true);
-        setIsLoginModalOpen(false);
-        setIsRegistering(false);
+    const tourismRoutes = {
+        walking: [
+            { name: "Столбы", link: "/routes/stolby/" },
+            { name: "Ергаки", link: "/routes/ergaki/" },
+            { name: "Кутурчинское белогорье", link: "/routes/kuturcha/" },
+            { name: "Мининские столбы", link: "/routes/mininskie/" },
+        ],
+        water: [
+            { name: "Катунь", link: "/routes/katun/" },
+            { name: "Есауловка", link: "/routes/esaulovka/" },
+            { name: "Мана", link: "/routes/mana/" },
+            { name: "Рыбная", link: "/routes/rybnaya/" },
+        ],
+        mountain: [
+            { name: "Борус", link: "/routes/borus/" },
+            { name: "Арктру", link: "/routes/arctru/" },
+            { name: "Белуха", link: "/routes/beluha/" },
+            { name: "Казбек", link: "/routes/kazbek/" },
+        ],
     };
 
     return (
-        <React.Fragment>
-            <nav className="navbar">
-                <div className="container">
-                    <a href="/" className="navbar-brand">LOGO</a>
-                    <div className="navbar-wrap">
-                        <ul className="navbar-menu">
-                            <li className="dropdown">
-                                <a href="#" className="dropdown-toggle" onClick={toggleDropdown}>
-                                    Направления
-                                </a>
-                                {isDropdownOpen && (
-                                    <ul className="dropdown-menu open">
-                                        <li><a href="/walking_tourism/">Пеший туризм</a></li>
-                                        <li><a href="/water_tourism/">Водный туризм</a></li>
-                                        <li><a href="/mountain_tourism/">Горный туризм</a></li>
-                                    </ul>
-                                )}
-                            </li>
-                            <li><a href="/media/">Медиа</a></li>
-                            <li><a href="/medicine/">Медицина</a></li>
-                            <li><a href="/about_project/">Контакты</a></li>
-                            <li><a href="/account/login/">Профиль</a></li>
-                            <li>
-                                <div className={`theme-switch-button ${theme}`} onClick={toggleTheme}>
-                                    <div className="icon-wrapper">
-                                        <img
-                                            src={theme === "light" ? "/static/image/sun.png" : "/static/image/moon.png"}
-                                            alt="theme icon"
-                                            className="theme-icon"
-                                        />
-                                    </div>
+        <nav className="navbar">
+            <div className="container">
+                <a href="/" className="navbar-brand">LOGO</a>
+                <div className="navbar-wrap">
+                    <ul className="navbar-menu">
+                        <li
+                            className="dropdown"
+                            onMouseEnter={() => setIsDropdownOpen(true)}
+                            onMouseLeave={() => setIsDropdownOpen(false)}
+                        >
+                            <a href="#" className="dropdown-toggle" onClick={toggleDropdown}>
+                                Направления
+                            </a>
+                            {isDropdownOpen && (
+                                <ul className="dropdown-menu open">
+                                    <li className="tourism-item-container">
+                                        <a href="/walking_tourism/">Пеший туризм</a>
+                                        <ul className="routes-dropdown">
+                                            {tourismRoutes.walking.map((route, i) => (
+                                                <li key={i}><a href={route.link}>{route.name}</a></li>
+                                            ))}
+                                        </ul>
+                                    </li>
+                                    <li className="tourism-item-container">
+                                        <a href="/water_tourism/">Водный туризм</a>
+                                        <ul className="routes-dropdown">
+                                            {tourismRoutes.water.map((route, i) => (
+                                                <li key={i}><a href={route.link}>{route.name}</a></li>
+                                            ))}
+                                        </ul>
+                                    </li>
+                                    <li className="tourism-item-container">
+                                        <a href="/mountain_tourism/">Горный туризм</a>
+                                        <ul className="routes-dropdown">
+                                            {tourismRoutes.mountain.map((route, i) => (
+                                                <li key={i}><a href={route.link}>{route.name}</a></li>
+                                            ))}
+                                        </ul>
+                                    </li>
+                                </ul>
+                            )}
+                        </li>
+                        <li><a href="/media/">Медиа</a></li>
+                        <li><a href="/medicine/">Медицина</a></li>
+                        <li><a href="/about_project/">Контакты</a></li>
+                        <li><a href="/account/login/">Профиль</a></li>
+                        <li>
+                            <div className={`theme-switch-button ${theme}`} onClick={toggleTheme}>
+                                <div className="icon-wrapper">
+                                    <img
+                                        src={theme === "light" ? "/static/image/sun.png" : "/static/image/moon.png"}
+                                        alt="theme icon"
+                                        className="theme-icon"
+                                    />
                                 </div>
-                            </li>
-                        </ul>
-                    </div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
-            </nav>
-        </React.Fragment>
+            </div>
+        </nav>
     );
 };
